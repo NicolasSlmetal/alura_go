f6ef33fb0826dc3c455f3774c266b8dbdad74ffb
Nicolas <nlimadasilvabrito@gmail.com>
2024-12-10 13:47:35 -0300

    feat: add file based analisys of sites availability

--- src/main/main.go
+++ src/main/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -37,7 +40,7 @@ func main() {
 }
 
 func monitor() {
-	websites := []string{"http://youtube.com.br", "http://www.google.com", "http://www.facebook.com"}
+	websites := readSitesFromFile()
 	for _, website := range websites {
 		monitorSite(website)
 		time.Sleep(5 * time.Second)
@@ -45,11 +48,67 @@ func monitor() {
 }
 
 func monitorSite(website string) {
-	resp, _ := http.Get(website)
+	resp, err := http.Get(website)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 	if resp.StatusCode == 200 {
 		fmt.Println(website, "is online")
 	} else {
 		fmt.Println(website, "is offline")
 		fmt.Println("Status code:", resp.StatusCode)
 	}
+}
+
+func readSitesFromFile() []string {
+	var sites []string
+
+	file, err := os.Open("sites.txt")
+	if err != os.ErrNotExist {
+		sites, err = createSitesFile()
+		if err != nil {
+			fmt.Println("Error:", err)
+			return nil
+		}
+		return sites
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	err = nil
+	for {
+
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		sites = append(sites, strings.TrimSpace(line))
+	}
+
+	return sites
+}
+
+func createSitesFile() ([]string, error) {
+	defaultSites := []string{
+		"https://www.google.com",
+		"https://www.youtube.com",
+		"https://www.reddit.com",
+	}
+	file, err := os.Create("sites.txt")
+
+	defer file.Close()
+
+	for _, site := range defaultSites {
+		_, err = file.WriteString(site + "\n")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return defaultSites, nil
 }
